1797fb46dff3885c628dbf2f0da36d9365ecad61
Chaoyu <631479724@qq.com>
2021-03-31 18:29:12 +0800

    testing

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -11,8 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
-    private void test(){
-        System.out.println("Hello,World")
-    }
 }
